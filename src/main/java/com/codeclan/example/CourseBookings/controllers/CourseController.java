0918a1b329069632b9276cd1ac25657c60b84cc7
evelyn-sz <evelynszarek@gmail.com>
2021-02-25 14:09:54 +0000

    Route getCoursesByRating works

--- src/main/java/com/codeclan/example/CourseBookings/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/CourseBookings/controllers/CourseController.java
@@ -1,4 +1,29 @@
 package com.codeclan.example.CourseBookings.controllers;
 
+import com.codeclan.example.CourseBookings.models.Course;
+import com.codeclan.example.CourseBookings.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value="/courses")
+    public ResponseEntity<List<Course>> getCoursesByRating(
+            @RequestParam(name="rating", required = false) Integer rating
+    ) {
+        if (rating != null) {
+            return new ResponseEntity<>(courseRepository.findByRating(rating), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
 }
